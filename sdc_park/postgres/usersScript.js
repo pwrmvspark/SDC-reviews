29e75139a316330d1ae776f7160f76eecf640e17
pwrmvspark <danielparkonline@gmail.com>
2019-08-16 10:57:29 -0700

    docker added

--- sdc_park/postgres/usersScript.js
+++ sdc_park/postgres/usersScript.js
@@ -1,3 +1,4 @@
+//postgres pwrmvs$ psql -h ec2-52-90-105-154.compute-1.amazonaws.com -p 5432 -U postgres -W docker -d reviews -c "\copy users FROM /Users/pwrmvs/SDC-reviews/sdc_park/postgres/users2.csv CSV HEADER;"
 const faker = require('faker')
 const { appendFileSync } = require('fs')
 
